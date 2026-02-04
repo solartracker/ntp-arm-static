15dcd251b963fdae7cb01d7a2e0c4bce90bcb727
Richard Elwell <229418400@users.noreply.github.com>
2026-02-04 09:33:03 -0500

    fix libcap build

--- ntp-arm-musl.sh
+++ ntp-arm-musl.sh
@@ -1022,7 +1022,7 @@ if [ ! -f "${PKG_SOURCE_SUBDIR}/__package_installed" ]; then
 
     $MAKE prefix="${PREFIX}" \
           inc_prefix="${PREFIX}/usr" \
-          LIBDIR="${PREFIX}/lib" \
+          lib="lib" \
           BUILD_CC="gcc" \
           BUILD_CPPFLAGS="-I./libcap/include"
 
