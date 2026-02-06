63aca260a596ab68d6753c4ab1c0676d7225ad19
Richard Elwell <229418400@users.noreply.github.com>
2026-02-06 15:57:55 -0500

    fix libcap

--- ntp-arm-asuswrt.sh
+++ ntp-arm-asuswrt.sh
@@ -81,9 +81,11 @@ case "${HOST_CPU}" in
         ;;
 esac
 
-BUILD_DIR="${PARENT_DIR}/${TOOLCHAINS_SUBDIR}-build/${CROSSBUILD_SUBDIR}"
-SRC_ROOT="${BUILD_DIR}/src/${PKG_ROOT}"
-PACKAGER_ROOT="${BUILD_DIR}/packager/${PKG_ROOT}/${PKG_ROOT}-${PKG_ROOT_VERSION}"
+#BUILD_DIR="${PARENT_DIR}/${TOOLCHAINS_SUBDIR}-build/${CROSSBUILD_SUBDIR}"
+#SRC_ROOT="${BUILD_DIR}/src/${PKG_ROOT}"
+#PACKAGER_ROOT="${BUILD_DIR}/packager/${PKG_ROOT}/${PKG_ROOT}-${PKG_ROOT_VERSION}"
+SRC_ROOT="${CROSSBUILD_DIR}/src/${PKG_ROOT}"
+PACKAGER_ROOT="${CROSSBUILD_DIR}/packager/${PKG_ROOT}/${PKG_ROOT}-${PKG_ROOT_VERSION}"
 
 MAKE="make -j$(grep -c ^processor /proc/cpuinfo)" # parallelism
 #MAKE="make -j1"                                  # one job at a time
