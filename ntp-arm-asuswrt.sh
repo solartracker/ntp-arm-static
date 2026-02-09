b9c4c56282ac860cdedeb346027a29ac4677bb0a
Richard Elwell <229418400@users.noreply.github.com>
2026-02-09 10:24:35 -0500

    cleanup build

--- ntp-arm-asuswrt.sh
+++ ntp-arm-asuswrt.sh
@@ -1244,7 +1244,7 @@ if [ ! -f "${PKG_BUILD_SUBDIR}/__package_installed" ]; then
     READELF=readelf
     CFLAGS_COMMON="-O2 -g"
     export CFLAGS="${CFLAGS_COMMON} -std=gnu99"
-    export CXXFLAGS="${CFLAGS_COMMON} -std=gnu++98"
+    export CXXFLAGS="${CFLAGS_COMMON} -std=gnu++98 -Wno-pedantic"
     export LDFLAGS="-L${PREFIX}/lib -Wl,--gc-sections"
     export CPPFLAGS="-I${PREFIX}/include -D_GNU_SOURCE"
 
@@ -1268,7 +1268,7 @@ if [ ! -f "${PKG_BUILD_SUBDIR}/__package_installed" ]; then
         --disable-libstdcxx-pch \
         --enable-target-optspace \
         --with-arch=armv7-a --with-tune=cortex-a9 --with-float=soft --with-abi=aapcs-linux \
-        --enable-cxx-flags='-std=gnu++98 -march=armv7-a -mtune=cortex-a9 -marm -mfloat-abi=soft -mabi=aapcs-linux' \
+        --enable-cxx-flags='-std=gnu++98 -Wno-pedantic -march=armv7-a -mtune=cortex-a9 -marm -mfloat-abi=soft -mabi=aapcs-linux' \
     || handle_configure_error $?
 
     $MAKE CXXFLAGS="${CXXFLAGS}" all-target-libatomic
